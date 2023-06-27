5ddab8418233fcbb509b9a9fe1d95c845d2bf2d9
Joao-ava <ajoao781238@gmail.com>
2023-06-27 09:21:13 -0300

    add: search field by label close #1

--- src/components/Add/SelectField/index.tsx
+++ src/components/Add/SelectField/index.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef } from 'react'
+import React, { forwardRef, useState } from 'react'
 import { IconType } from 'react-icons'
 import { BiText, BiHash, BiCalendarAlt, BiDownArrowCircle } from 'react-icons/bi'
 import { Field, FieldType } from '../../../types.ts'
@@ -14,21 +14,25 @@ const iconsByType: Record<FieldType, IconType> = {
   date: BiCalendarAlt,
   array: BiDownArrowCircle
 }
-const SelectFieldFunction: React.ForwardRefRenderFunction<HTMLDivElement, SelectFieldProps> = ({ fields, onSelectField }, ref) => (
-  <div className="rsql-box rsql-select-field" ref={ref}>
-    <input className="rsql-input" name="rsql-field-search" placeholder="Filter by..." />
-    <div>
-      {fields.map((item) => {
-        const Icon = iconsByType[item.type]
-        return (
-        <div className='rsql-select-field-option' key={item.selector} onClick={() => onSelectField(item.selector)}>
-          <Icon />
-          <p>{item.label}</p>
-        </div>
-      )})}
+const SelectFieldFunction: React.ForwardRefRenderFunction<HTMLDivElement, SelectFieldProps> = ({ fields, onSelectField }, ref) => {
+  const [search, setSearch] = useState('')
+  const items = fields.filter((item) => item.label.toLowerCase().includes(search.toLowerCase()))
+  return (
+    <div className="rsql-box rsql-select-field" ref={ref}>
+      <input value={search} onChange={(e) => setSearch(e.target.value)} className="rsql-input" name="rsql-field-search" placeholder="Filter by..." />
+      <div>
+        {items.map((item) => {
+          const Icon = iconsByType[item.type]
+          return (
+            <div className='rsql-select-field-option' key={item.selector} onClick={() => onSelectField(item.selector)}>
+              <Icon />
+              <p>{item.label}</p>
+            </div>
+          )})}
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 const SelectField = forwardRef(SelectFieldFunction)
 
